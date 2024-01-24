45f0d6fef8c0e7344892be54d84efa5664ed1df2
SoftRiseGames <98230446+SoftRiseGames@users.noreply.github.com>
2024-01-24 17:04:29 +0300

    reset

--- Assets/Scripts/SceneControl.cs
+++ Assets/Scripts/SceneControl.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
+using System.IO;
 public class SceneControl : MonoBehaviour
 {
     int isCarSkipped;
@@ -18,5 +20,50 @@ public class SceneControl : MonoBehaviour
     {
         SceneManager.LoadScene(1);
     }
+
+    public void DeleteJsonFiles(string folderPath)
+    {
+        try
+        {
+            // Check if the specified directory exists
+            if (Directory.Exists(folderPath))
+            {
+                // Search for JSON files within the directory and its subdirectories
+                string[] jsonFiles = Directory.GetFiles(folderPath, "*.json", SearchOption.AllDirectories);
+
+                // Loop through each JSON file found and delete it
+                foreach (var jsonFilePath in jsonFiles)
+                {
+                    File.Delete(jsonFilePath);
+                    Console.WriteLine($"Deleted: {jsonFilePath}");
+                }
+
+                Console.WriteLine("All JSON files have been deleted.");
+            }
+            else
+            {
+                Console.WriteLine("Directory not found!");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    public void ClickDeleter()
+    {
+#if UNITY_EDITOR
+        DeleteJsonFiles(Application.dataPath + "/JsonFolder");
+        DeleteJsonFiles(Application.dataPath + "/JsonHumanFolder");
+
+#else
+        DeleteJsonFiles(Application.persistentDataPath + "/JsonFolder");
+        DeleteJsonFiles(Application.persistentDataPath + "/JsonHumanFolder");
+#endif
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene(0);
+    }
+
 
 }
