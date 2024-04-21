900c9c7a7f13baac5ee75812062c4e0be9bbf58e
aysebetulaktan <aysebetulaktan@ogrenci.beykoz.edu.tr>
2024-04-21 23:27:18 +0300

    respect bar animation

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,26 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;//ben
+using TMPro;
 public class UIController : MonoBehaviour
 {
-    public static UIController Instance;
 
-    private void Awake()
+    public TMP_Text respectText;
+    public float moveSpeed;
+    public float lifetime = 1f;
+    private RectTransform myRect;
+
+    public HumanLister humanLister;
+
+    public void UpdateRespectCounter(int newRespectValue)
     {
-            Instance = this;
+        
     }
 
-    public GameObject MoneyText;//ben
 
     void Start()
     {
-        MoneyText = GameObject.FindObjectOfType<GameObject>();//ben
+        Destroy(gameObject, lifetime);
+
+        myRect = GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        myRect.anchoredPosition += new Vector2(0f, -moveSpeed * Time.deltaTime);
     }
 }
