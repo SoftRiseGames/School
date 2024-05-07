aac545d05a4db6f01e5228ab6adcb1901e414cad
SoftRiseGames <98230446+SoftRiseGames@users.noreply.github.com>
2024-05-07 22:39:36 +0300

    last

--- Assets/Scripts/Specials/HumanSpecials.cs
+++ Assets/Scripts/Specials/HumanSpecials.cs
@@ -37,8 +37,6 @@ public class HumanSpecials : MonoBehaviour
     {
         HumanPersonalities();
         SpecialsTextManager();
-        Debug.Log(GameObject.Find("Limits").transform.GetChild(0));
-        
     }
     public void HumanPersonalities()
     {
